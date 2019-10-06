5744916f66b7c5d74379dab8d8f10012c58081ec
Алина <56219465+matveevaayu@users.noreply.github.com>
2019-10-06 20:59:24 +0700

    homework 1.1

---
+++ MainClassTest.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass{
+    @Test
+    public void testGetLocalNumber() {
+        Assert.assertTrue("getLocalNumber != 14",getLocalNumber() == 14);
+    }
+}
