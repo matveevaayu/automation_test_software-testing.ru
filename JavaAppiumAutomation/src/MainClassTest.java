905a31268a3cc6c729ac291ddd7fde217c81d78e
Alina Matveeva <matveevaayu@gmail.com>
2019-10-06 21:09:16 +0700

    homework 1.1

--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -6,6 +6,5 @@ public class MainClassTest extends MainClass{
     @Test
     public void testGetLocalNumber() {
         Assert.assertTrue("getLocalNumber != 14",getLocalNumber() == 14);
-        System.out.println("Hello!");
     }
 }
