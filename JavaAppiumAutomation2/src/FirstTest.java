3e901ac3792821193902226a1394317224792d88
Alina Matveeva <matveevaayu@gmail.com>
2019-10-20 18:27:39 +0700

    homework 3.1
    Написать функцию, которая проверяет наличие текста “Search…” в строке
    поиска перед вводом текста и помечает тест упавшим, если такого текста
    нет.

--- JavaAppiumAutomation2/src/FirstTest.java
+++ JavaAppiumAutomation2/src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -30,9 +35,69 @@ public class FirstTest {
     {
         driver.quit();
     }
+
     @Test
-    public void firstTest()
+    public void testComparePlaceholderText()
     {
-        System.out.println("first test run");
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia input",
+                5
+        );
+        WebElement search_container = waitForElementPresent(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Cannot find Search input",
+                5
+        );
+
+        String placeholder_text = search_container.getAttribute("text");
+
+        Assert.assertEquals(
+                "We see unexpected placeholder!",
+                "Search…",
+                placeholder_text
+        );
+
+    }
+
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message){
+        return  waitForElementPresent(by, error_message, 5);
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, 5);
+        element.click();
+        return element;
+    }
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, 5);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, error_message, 5);
+        element.clear();
+        return element;
     }
 }
